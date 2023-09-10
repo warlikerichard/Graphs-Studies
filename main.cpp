abbe4f3a57e2d82edd8b15693222f709e5802faa
warlikerichard <warlikerichard@hotmail.com>
2023-09-10 13:48:51 -0300

    added sort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <list>
-#include <stack>
 #include <vector>
 #include <map>
 
 using namespace std;
+
+class Vertice{
+    public:
+    int value;
+    bool visitado = false;
+
+    Vertice(int v){
+        value = v;
+    }
+};
 
 // A classe abaixo define um grafo que tem como principal dado um conjunto de maps, que representam vértices adjacentes.
 // Decidi criar um ponteiro de maps, pois assim posso atribuir uma chave a um evento do jogo. Ex: 2 = "Enter room".
@@ -21,12 +30,21 @@ private:
 public:
     Grafo(int V);
     void adicionarAresta(int v, int w);
+    list<Vertice> ordenacaoTopologica();
+    void visite(Vertice& v, list<Vertice>* o);
+    int getSize(){
+        return V;
+    }
     Vertices* getList(){
         return adj;
     }
 
     Vertices getVertices(){
         return vertices;
+    }
+
+    Vertices getAdjacentes(int n){
+        return adj[n];
     }
 };
 
@@ -51,8 +69,41 @@ void Grafo::adicionarAresta(int v, int w) {
     adj[v][w] = getVertices()[w];
 }
 
-int main() {
+void Grafo::visite(Vertice& v, list<Vertice>* o){
+    if(!v.visitado){
+        v.visitado = true;
+        Vertices adjacents = getAdjacentes(v.value);
 
+        for(auto it=adjacents.begin(); it!=adjacents.end(); it++){ // Para cada vertice V adjacente a v...
+            Vertice V(it->first);
+            visite(V, o);
+        }
+        o->push_front(V); // Adicione V ao inicio da lista o
+    }
+}
+
+list<Vertice> Grafo::ordenacaoTopologica(){
+    list<Vertice> * o;
+    o = new list<Vertice>();
+    Vertices* adj = getList();
+    for(int i=0; i<getSize(); i++){ // Para cada vertice em adj...
+        Vertices* vList = getList();
+        bool depends = false;
+
+        for(int j=0; j<getSize(); j++){ // se vertice for encontrado em algum elemento da lista, é dependente...
+            if(vList[j].find(i) != vList[j].end()) depends = true;
+        }
+
+        if(!depends){
+            Vertice vertice(i);
+            visite(vertice, o);
+        }
+        
+    }
+    return *o;
+}
+
+int main() {
     Grafo g(7);
     g.adicionarAresta(0, 1);
     g.adicionarAresta(0, 3);
@@ -65,14 +116,20 @@ int main() {
 
     cout << '\n';
 
-    Grafo::Vertices* list = g.getList();
-    int evento = 0;
+    Grafo::Vertices* vList = g.getList();
+    int evento = 2;
     // Para cada i sendo um vértice que depende de vertices[evento]
     // Print vertice
     cout << "'" << g.getVertices()[evento] << "' é necessário para:\n";
-    for(auto i=list[evento].begin(); i!=list[evento].end(); i++){
+    for(auto i=vList[evento].begin(); i!=vList[evento].end(); i++){
         cout << g.getVertices()[i->first] << "\n";
     }
 
+    list<Vertice> listFinal = g.ordenacaoTopologica();
+    cout << listFinal.front().value;
+
+    for(auto it = listFinal.begin(); it != listFinal.end(); ++it){
+        cout << it->value << " ";
+    }
     return 0;
 }
